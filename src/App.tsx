8051c5a5235a16c7c8b85ea23aeebd3db477b606
Jacob Bruce <brucejac@sheridancollege.ca>
2020-10-10 11:20:09 -0400

    Styled components, set up API calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
+import Axios from 'axios';
 import React, { useState } from 'react';
 import './App.css';
 import Dashboard from './Dashboard';
+import image from './images/snowing.jpg';
 
-//Design: https://dribbble.com/vowles
+//Design: https://dribbble.com/vowles/projects/491714-Weather-project
 
+//https:codepen.io/brandonkennedy/pen/yGjsi
 function App() {
     const [searchQuery, changeSearchQuery] = useState('');
+    const [weather, setWeather] = useState({});
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        const { lat, lon } = (
+            await Axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&units=metric&APPID=${process.env.REACT_APP_WEATHER_KEY}`
+            )
+        ).data.coord;
+        const { data } = await Axios.get(
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_KEY}`
+        );
+        console.log(data);
+        setWeather(data);
+        changeSearchQuery('');
+    };
 
     return (
         <div className='App'>
-            <input
-                type='text'
-                className='search'
-                value={searchQuery}
-                onChange={(e) => changeSearchQuery(e.target.value)}
-            />
-            <Dashboard />
+            <div className='info-container'>
+                <div className='search-container'>
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type='text'
+                            className='search'
+                            value={searchQuery}
+                            onChange={(e) => changeSearchQuery(e.target.value)}
+                        />
+                    </form>
+                </div>
+                <Dashboard weather={weather} />
+            </div>
+            <img className='image' src={image} alt='' />
         </div>
     );
 }
--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -2,14 +2,34 @@ import React from 'react';
 import './Dashboard.css';
 
 export default function Dashboard() {
+    const monthNames = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ];
+
+    const date = new Date();
+    const month = monthNames[date.getMonth()];
+    const day = date.toDateString().slice(0, 3);
+
     return (
         <div className='Dashboard'>
             <h1 className='Dashboard-date'>
-                mon, 13 <br /> November
+                {day} {date.getDate()} <br /> {month}
             </h1>
             <div className='Dashboard-temperature-container'>
                 <h1 className='Dashboard-temperature'>-10°</h1>
             </div>
+            <div className='Dashboard-weekly-container'></div>
         </div>
     );
 }

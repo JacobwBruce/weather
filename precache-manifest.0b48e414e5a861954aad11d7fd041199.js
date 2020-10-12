f9f078337cd2269531e3b4cd107ac392e1e32b15
Jacob Bruce <brucejac@sheridancollege.ca>
2020-10-12 09:24:24 -0400

    Updates

---
+++ precache-manifest.0b48e414e5a861954aad11d7fd041199.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c8929707b3b600c434729e3bdbd837",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "08bc3c77543196f46c41",
+    "url": "/weather/static/css/main.81e32baa.chunk.css"
+  },
+  {
+    "revision": "471eae58b1bd091a1367",
+    "url": "/weather/static/js/2.a91bf632.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.a91bf632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08bc3c77543196f46c41",
+    "url": "/weather/static/js/main.4cdcae52.chunk.js"
+  },
+  {
+    "revision": "1557ce1959b3418744e2",
+    "url": "/weather/static/js/runtime-main.80a89714.js"
+  },
+  {
+    "revision": "e94e328132cb1e611466a8fc1ede5144",
+    "url": "/weather/static/media/atmosphere.e94e3281.jpeg"
+  },
+  {
+    "revision": "48a2b5b817e06ec07a55f2d4e4abd290",
+    "url": "/weather/static/media/clear.48a2b5b8.jpeg"
+  },
+  {
+    "revision": "d5c3e4547d5dbe78b87e3db6054c5fb7",
+    "url": "/weather/static/media/cloudy.d5c3e454.jpeg"
+  },
+  {
+    "revision": "6e18a2f424431cf4cca34accb410c0be",
+    "url": "/weather/static/media/rain.6e18a2f4.jpeg"
+  },
+  {
+    "revision": "ee32453458655df4c7ae885702432a2f",
+    "url": "/weather/static/media/thunderstorm.ee324534.jpeg"
+  }
+]);
